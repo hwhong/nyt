e6c09c2cf038cb8cb14ab1cff9605d94e000724b
warren <hwhong1129@gmail.com>
2020-09-08 14:04:04 -0700

    structure in place

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,55 @@
 import React from "react";
 import styles from "./main.module.css";
+import logoPath from "./asset/logo.svg";
 
 function App() {
-  return <div className={styles.root}></div>;
+  const tabs = [
+    "",
+    "World",
+    "U.S.",
+    "Politics",
+    "N.Y.",
+    "Business",
+    "Opinion",
+    "Tech",
+    "Science",
+    "Health",
+    "Sports",
+    "Arts",
+    "Books",
+    "Style",
+    "Food",
+    "Travel",
+    "Magazine",
+    "T Magazine",
+    "Real Estate",
+    "Video",
+    "",
+  ];
+  return (
+    <div className={styles.root}>
+      <div className={styles.topBanner}>
+        <div className={styles.left}>Tuesday, September 8, 2020</div>
+        <div className={styles.mid}>
+          <a className={styles.imageContainer}>
+            <img src={logoPath} className={styles.image} />
+          </a>
+        </div>
+        <div className={styles.right}>Today's Paper</div>
+        <div className={styles.tabs}>
+          {tabs.map((tab) => (
+            <p className={styles.tabName}>{tab}</p>
+          ))}
+        </div>
+      </div>
+      <div className={styles.midBanner}></div>
+      <div className={styles.content}></div>
+    </div>
+  );
 }
 
 export default App;
+
+// 1fr means that it will take up the available space left.
+// grid-column: 1 / -1; makes content span entire row
+// grid-template-columns: repeat(21, minmax(0, 1fr)); for equally sized width columns
